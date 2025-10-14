b3bb8dfd210a3a22bc7bf3cb9155e05a31a2bbd5
Carlos Martinez <carlos.martinez@medwing.com>
2025-10-14 22:15:26 +0200

    bla

---
+++ space_ships/main.lua
@@ -0,0 +1,17 @@
+ship = {
+  x = 100,
+  y = 100,
+}
+
+local function load()
+end
+
+local function update(dt)
+end
+
+local function draw()
+end
+
+love.load = load
+love.update = update
+love.draw = draw
